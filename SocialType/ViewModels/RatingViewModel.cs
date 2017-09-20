1026ccb56ca89f22aeb5c7023a0cbc81f0508e0e
andriuzza <andrius.butkevicius1@gmail.com>
2017-09-20 21:53:15 +0300

    Added ability to put drinks rating in a database, at this time without validations. Soon will be updated

---
+++ SocialType/ViewModels/RatingViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialType.Controllers
+{
+    public class RatingViewModel
+    {
+        [Required]
+        public double Rating { get; set; }
+        [Required]
+        public int HowManyTimes { get; set; }
+    }
+}

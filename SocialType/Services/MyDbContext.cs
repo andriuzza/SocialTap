ecdf8b71c7e9c6b779d38fa64715dac5fda2b9f4
minbar <mindaugas.barcauskas@gmail.com>
2017-10-08 20:55:18 +0300

    Login unit tests.

--- SocialType/Services/MyDbContext.cs
+++ SocialType/Services/MyDbContext.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
-
+using SqlProviderServices = System.Data.Entity.SqlServer.SqlProviderServices;
 namespace SocialType.Models
 {
     public class MyDbContext : DbContext

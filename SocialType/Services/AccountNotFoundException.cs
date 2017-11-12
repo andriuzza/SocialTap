0449eaada0679dc8f5e2353c7d54b84da24881a8
minbar <mindaugas.barcauskas@gmail.com>
2017-11-13 00:11:06 +0200

    changed Account Exception

---
+++ SocialType/Services/AccountNotFoundException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Windows;
+
+namespace SocialType.Services
+{
+    public class AccountNotFoundException : Exception
+    {
+        public AccountNotFoundException()
+        {
+            MessageBox.Show("account does not exist");
+        }
+
+        public AccountNotFoundException(String message) : base(message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+}
